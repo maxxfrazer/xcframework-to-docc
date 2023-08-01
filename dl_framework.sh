068a0768400487c4e7f77fd188d2f25b0affdd18
Max Cobb <maxxc@mac.com>
2023-08-01 13:46:11 +0100

    allow redirects for downloads

--- dl_framework.sh
+++ dl_framework.sh
@@ -6,7 +6,7 @@ frameworkname=$1
 url=$(grep -m 1 "https.*$frameworkname.*.xcframework.zip" Package.swift | sed -E "s/.*(https.*$frameworkname.*.xcframework.zip).*/\1/")
 
 # Download the xcframework zip file
-curl -O $url
+curl -L -O $url
 
 # Extract the contents of the zip file to the current directory
 unzip $frameworkname.xcframework.zip
